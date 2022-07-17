bbe9331025d94961e22430f49d1008b4433a21a0
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2022-07-16 22:12:51 -0500

    added new method to collect valid pdf files in folder

--- src/PayrollReportSorter.java
+++ src/PayrollReportSorter.java
@@ -1,10 +1,37 @@
 import java.io.File;
+import java.util.ArrayList;
 
 public class PayrollReportSorter
 {
 	public static void main(String[] args)
 	{
-		new PayrollPDFToText(new File("src/Payroll Folder/Input/report - cv.pdf"));
-		new PayrollPDFToText(new File("src/Payroll Folder/Input/report - wl.pdf"));
+		for(int i = 0; i < getInputFiles().toArray().length; i++){
+			PayrollPDFToText file = new PayrollPDFToText(new File("src/Payroll Folder/Input/" + getInputFiles().toArray()[i]));
+			PayrollSorter sorter = new PayrollSorter(file.getNewFile(), file.getFacilityName(),file.getPayrollDate());
+
+			System.out.println(sorter.displayInfo());
+		}
+	}
+
+	// Returns an ArrayList of valid input files
+	private static ArrayList<String> getInputFiles(){
+		File directory = new File("src/Payroll Folder/Input/");
+		String[] files = directory.list();
+		ArrayList<String> validFiles = new ArrayList<>();
+
+		if(files != null){
+			for(String file : files){
+				String fileExtension = file.substring(file.length() - 4);
+
+				if(fileExtension.contains(".")){
+					String validFile = fileExtension.contains(".pdf") ?  file : null;
+					if(validFile != null){
+						validFiles.add(validFile);
+					}
+				}
+			}
+		}
+
+		return validFiles;
 	}
 }
