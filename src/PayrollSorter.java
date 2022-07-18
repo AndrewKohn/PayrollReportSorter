7608368fd7ee44d39d91f2f52d5236487e9d27b0
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2022-07-18 05:59:18 -0500

    Added a majority of the class functionalities.

--- src/PayrollSorter.java
+++ src/PayrollSorter.java
@@ -1,171 +1,177 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class PayrollSorter
 {
 	private final File file;
 	private final String facilityName;
 	private final String payrollDate;
+	private final Set<Integer> employeeIDs = new HashSet<>();
+	private final Map<Integer, ArrayList<String>> employeeMap = new HashMap<>();
+	private final Set<String> missedClockOuts = new HashSet<>();
 
 	public PayrollSorter(File newFile, String facilityName, String payrollDate){
 		this.file = newFile;
 		this.facilityName = facilityName;
 		this.payrollDate = payrollDate;
 
+		collectEmployeeIDs();
+		setEmployeeMap();
+		setMissedClockOuts();
+	}
 
-		System.out.println("***********************************************************************" + facilityName);
+	public void displayInfo(){
+		System.out.println("*****************************************************************************************");
+		System.out.println(getFacilityName() + " : number of clocked staff: " + getEmployeeIDs().size() + " ||  Payroll date: " + getPayrollDate());
+		System.out.println("*****************************************************************************************");
 
-		for(Map.Entry<Integer, String> employee : createEmployeesMap().entrySet()){
-			createHeader(employee.getKey(), employee.getValue());
-			System.out.println();
-			for(int i = 0; i < getFileLinePoints(createEmployeesMap()).length; i++){
-				ArrayList<String> scannedLines = scanLines(getFileLinePoints(createEmployeesMap())[i][0], getFileLinePoints(createEmployeesMap())[i][1]);
-
-				String[] temp = scannedLines.get(0).split(" ");
-//				System.out.println(Integer.parseInt(temp[3].replace("#", "")));
-//				System.out.println(scannedLines.get(0));
-
-				if(Integer.parseInt(temp[3].replace("#", "")) == employee.getKey()){
-//					System.out.println(Integer.parseInt(temp[3].replace("#", "")));
-					for(String line : scannedLines){
-						System.out.println(line);
-					}
-				}
+		for(Map.Entry<Integer, ArrayList<String>> employee : employeeMap.entrySet()){
+			System.out.println(employee.getKey());
+			for(String employeeData : employee.getValue()){
+				System.out.println("    " + employeeData);
 			}
-
-			System.out.println("===========================================");
+			System.out.println("===============");
 		}
-
-		System.out.println("Missed Clock Outs:");
-		for(String missedClockOutLine : scanMissingClockOut()){
-			System.out.println(missedClockOutLine);
-		}
-		System.out.println("***********************************************************************");
 	}
 
-	public String displayInfo(){
-		return facilityName + " : " + payrollDate + "  |  " + file;
-	}
-
-	private ArrayList<String> scanLines(int entryPoint, int exitPoint){
-		int lineCount = 0;
-		int weekCount = 1;
-
-		ArrayList<String> scannedLines = new ArrayList<>();
-
+	private void collectEmployeeIDs(){
 		try{
 			Scanner scanner = new Scanner(file);
 			while(scanner.hasNextLine()){
 				String str = scanner.nextLine();
-				lineCount++;
-				if(lineCount >= entryPoint && lineCount <= exitPoint){
 
-					if(str.contains("Missing") || str.contains("Page") || str.contains("Time") || str.contains("From")){
-						continue;
-
-					}else if(str.contains("In Out Hours Note")){
-						scannedLines.add(str + "    ----|--  WEEK " + weekCount);
-						weekCount++;
-					}else{
-						scannedLines.add(str);
-					}
-				}
-
-
-			}
-		}catch (IOException e){
-			e.printStackTrace();
-		}
-
-		return scannedLines;
-	}
-
-	private ArrayList<String> scanMissingClockOut(){
-		ArrayList<String> missedClockOuts = new ArrayList<>();
-
-		try{
-			Scanner scanner = new Scanner(file);
-			while(scanner.hasNextLine()){
-				String str = scanner.nextLine();
-				if(str.contains("Missing")){
-						missedClockOuts.add(str);
+				if(str.contains("#")){
+					String[] temp = str.split(" ");
+					setEmployeeIDs(Integer.parseInt(temp[3].replace("#", "")));
 				}
 			}
+			scanner.close();
 		}catch (IOException e){
+			System.out.println("ERROR: Unable to get employee ID's...");
 			e.printStackTrace();
 		}
-
-		return missedClockOuts;
 	}
 
-//	Retrieves line positions of each employee's information within the file
-	private int[][] getFileLinePoints(Map<Integer, String> employees){
+	// Collects data in file at the start of the passed entry and stops collecting at the exit
+	private ArrayList<String> collectFileLineData(int entryPoint, int exitPoint){
+		ArrayList<String> fileLineData = new ArrayList<>();
 		int fileLineCount = 0;
-		int amountOfEmployees = 0;
-		int[][] recordFileLinePoints = new int[employees.size()][2];
+		int weekCount = 1;
 
 		try{
 			Scanner scanner = new Scanner(file);
 			while(scanner.hasNextLine()){
 				String str = scanner.nextLine();
 				fileLineCount++;
-				if(str.contains("#")){
-					recordFileLinePoints[amountOfEmployees][0] = fileLineCount;		// line count of entry point
-				}
 
-				if(str.contains("Totals:")){
-					recordFileLinePoints[amountOfEmployees][1] = fileLineCount;		// line count of exit point
-					amountOfEmployees++;
+				if(fileLineCount >= entryPoint && fileLineCount <= exitPoint){
+					if(str.contains("Missing") || str.contains("Page") || str.contains("Time") || str.contains("From")){
+						continue;
+
+					}else if(str.contains("In Out Hours Note")){
+						fileLineData.add(str + "    ----|--  WEEK " + weekCount);
+						weekCount++;
+					}else{
+						fileLineData.add(str);
+					}
 				}
 			}
+			scanner.close();
 		}catch (IOException e){
+			System.out.println("ERROR: Unable to collect file line data...");
 			e.printStackTrace();
 		}
 
-		return recordFileLinePoints;
+		return fileLineData;
 	}
 
-	private Map<Integer, String> createEmployeesMap(){
-		Map<Integer, String> employees = new HashMap<>();
+	// Finds entry/exit points depending on current line number.
+	private int[][] collectFileDataPoints(Set<Integer> employees){
+		int[][] fileDataPoints = new int[employees.size()][2];
+		int employeeCount = 0;
+		int fileLineCount = 0;
 
 		try{
 			Scanner scanner = new Scanner(file);
 			while(scanner.hasNextLine()){
 				String str = scanner.nextLine();
+				fileLineCount++;
+
 				if(str.contains("#")){
-					String[] temp = str.split(" ");
+					fileDataPoints[employeeCount][0] = fileLineCount;
+				}
 
-					String name = temp[0].concat(" " + temp[1]);
-					int id =  Integer.parseInt(temp[3].replace("#", ""));
-
-					employees.put(id, name);
+				if(str.contains("Totals:")){
+					fileDataPoints[employeeCount][1] = fileLineCount;
+					employeeCount++;
 				}
 			}
+			scanner.close();
 		}catch (IOException e){
-			System.out.println("ERROR:  Error scanning for employees");
+			System.out.println("ERROR: Unable to collect file data...");
 			e.printStackTrace();
 		}
 
-		return employees;
+		return fileDataPoints;
 	}
 
-	private String getFacilityName(){
+	private void setEmployeeIDs(int id){
+		employeeIDs.add(id);
+	}
+
+	public Set<Integer> getEmployeeIDs(){
+		return employeeIDs;
+	}
+
+	public String getFacilityName(){
 		return	facilityName;
 	}
 
-	private String getPayrollDate(){
+	public String getPayrollDate(){
 		return payrollDate;
 	}
 
-	private void createHeader(Integer id, String name){
-		String employee = id.toString() + " : " + name;
+	public void setMissedClockOuts(){
+		try{
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()){
+				String str = scanner.nextLine();
 
-		System.out.println(employee);
-		System.out.println(getFacilityName());
-		System.out.println(getPayrollDate());
+				if(str.contains("Missing")){
+					missedClockOuts.add(str);
+				}
+			}
+		}catch (IOException e){
+			System.out.println("ERROR: Something happened setting Missing Clock-outs...");
+			e.printStackTrace();
+		}
+	}
+
+	public Set<String> getMissedClockOuts(){
+		return missedClockOuts;
+	}
+
+	// Sets map with 'key'=ID & 'value'= An arrayList of staff's hours.
+	private void setEmployeeMap(){
+		int[][] dataPoints = collectFileDataPoints(employeeIDs);
+
+		for(int[] dataPoint : dataPoints){
+			String[] temp = collectFileLineData(dataPoint[0], dataPoint[1]).get(0).split(" ");
+			int id = Integer.parseInt(temp[3].replace("#", ""));
+			for(int empID : employeeIDs){
+				if(id == empID){
+					employeeMap.put(empID, collectFileLineData(dataPoint[0], dataPoint[1]));
+				}
+			}
+		}
+	}
+
+	public Map<Integer, ArrayList<String>> getEmployeeMap(){
+		return employeeMap;
+	}
+
+	public File getFile(){
+		return file;
 	}
 }
