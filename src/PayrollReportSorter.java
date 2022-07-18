1e42e556ceb4ab5469c5370f5ee778b86bf0e5fe
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2022-07-18 05:57:19 -0500

    Adjusted Main to point to new PayrollFileConcat class

--- src/PayrollReportSorter.java
+++ src/PayrollReportSorter.java
@@ -1,12 +1,22 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class PayrollReportSorter
 {
+	private static Map<Integer,ArrayList<String>> map1;
+	private static Map<Integer,ArrayList<String>> map2;
+	private static File file1;
+	private static File file2;
+	private static Set<String> missedClockOut = new HashSet<>();
+
 	public static void main(String[] args)
 	{
 		if(getInputFiles().toArray().length > 0){
 			runApp(getInputFiles());
+			new PayrollFileConcat(map1, file1, map2, file2, missedClockOut);
 		}else{
 			System.out.println("ArrayList length: " + getInputFiles().toArray().length);
 			System.out.println("!!***********  No valid files in 'src/Payroll Folder/Input/' folder  ***********!!");
@@ -19,7 +29,15 @@ public class PayrollReportSorter
 			PayrollPDFToText file = new PayrollPDFToText(new File(filePath));
 			PayrollSorter sorter = new PayrollSorter(file.getNewFile(), file.getFacilityName(), file.getPayrollDate());
 
-			System.out.println(sorter.displayInfo());
+			if(map1 == null){
+				map1 = sorter.getEmployeeMap();
+				file1 = sorter.getFile();
+				missedClockOut = (sorter.getMissedClockOuts());
+			}else{
+				map2 = sorter.getEmployeeMap();
+				file2 = sorter.getFile();
+				missedClockOut.addAll(sorter.getMissedClockOuts());
+			}
 		}
 	}
 
